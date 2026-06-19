434acd9d822fdc47a24413d4c5e7394a1b34ca02
Michael Ross <mike.ross@miross.net>
2026-06-19 13:06:01 -0700

    feat: configure site URL and base path in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ascentiaci.github.io',
+  base: '/remote-attendance',
+});
